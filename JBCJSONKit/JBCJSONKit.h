b6fff6727f9e6ed0929cd9d8de5f5bec36bc5cc8
jacob berkman <jacob@87k.net>
2014-02-17 15:53:07 -0800

    Initial Commit

---
+++ JBCJSONKit/JBCJSONKit.h
@@ -0,0 +1,13 @@
+//
+//  JBCJSONKit.h
+//  JBCJSONKit
+//
+//  Created by jacob berkman on 2/17/14.
+//  Copyright (c) 2014 87k Networks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBCJSONKit : NSObject
+
+@end
---
+++ JBCJSONKit/JBCJSONKit.m
@@ -0,0 +1,13 @@
+//
+//  JBCJSONKit.m
+//  JBCJSONKit
+//
+//  Created by jacob berkman on 2/17/14.
+//  Copyright (c) 2014 87k Networks. All rights reserved.
+//
+
+#import "JBCJSONKit.h"
+
+@implementation JBCJSONKit
+
+@end
